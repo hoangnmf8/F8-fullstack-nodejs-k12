07fcc0fcd0252c52f702932649012780bdbee230
hoangnm.ndm <hoangnm.ndm@gmail.com>
2025-01-04 21:51:49 +0700

    Add BT

---
+++ Day27/ex04.js
@@ -0,0 +1,32 @@
+console.log(Math.min(10, -10, 2, 4));
+console.log(Math.max(10, -10, 2, 4));
+console.log(Math.pow(2, 4));
+
+let myNumber = 1234.5;
+console.log(Math.ceil(myNumber)); // lam tron len
+console.log(Math.floor(myNumber)); // lam tron xuong
+console.log(Math.round(myNumber)); // lam tron toi so nguyen gan nhat
+console.log(Math.random());
+
+/**
+ * 1. Tạo hàm random 1 tháng ngẫu nhiên (1-12) với xác suất bằng nhau.
+ * 2. Tạo hàm random trả về đáp án A,B,C,D ngẫu nhiên với xác suất bằng nhau.
+ * 3. Viết hàm tạo id ngẫu nhiên có n ký tự ngẫu nhiên trong khoảng [0-9], [A-Z], [a-z], n là tuỳ chọn nhập vào hàm, prefix là tuỳ chọn tiền tố từ người dùng
+ * example1: `prefix-a1cj`
+ */
+
+function randomMonth() {}
+
+function randomForQuizz() {
+	const arrTemplate = ["A", "B", "C", "D"]; // 0 - 3
+}
+
+function generatorId(prefix, n) {}
+
+/**
+ * Ý tưởng -> Vẽ lưu đồ giải thuật
+ * Bước 1:
+ * Bước 2:
+ */
+
+const templateString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
