06feaf5038cb6f53fff73015ead5b3eaecb4dd40
hoangnm.ndm <hoangnm.ndm@gmail.com>
2025-02-18 20:23:04 +0700

    Day 35

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,21 @@
 function LoginPage() {
-	return `
-  <div>
-    <h1>Login page</h1>
-  </div>
+	return /*html */ `
+    <form id="loginForm">
+			<h1>LOGIN</h1>
+			<div class="mb-3">
+				<label class="form-label" for="email">Email</label>
+				<input class="form-control" type="email" name="email" />
+			</div>
+
+			<div class="mb-3">
+				<label class="form-label" for="password">Password</label>
+				<input class="form-control" type="password" name="password" />
+			</div>
+
+			<div class="mb-3">
+				<button class="btn btn-primary w-100">Register</button>
+			</div>
+		</form>
 `;
 }
 
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,8 +1,45 @@
 function RegisterPage() {
-	return `
-  <div>
-    <h1>RegisterPage</h1>
-  </div>
+	// Buoc 1: Lay du lieu tu form
+	const registerForm = document.getElementById("registerForm");
+	registerForm.addEventListener("submit", function (event) {
+		event.preventDefault();
+
+		const user = Object.fromEntries(new FormData(registerForm));
+		console.log(user);
+
+		// Buoc 2: Gui du lieu di server
+		fetch("http://localhost:3000/register", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(user),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				if (data.accessToken) {
+					location.href = "./login.html";
+				}
+			})
+			.catch((err) => console.log(err));
+	});
+
+	return /*html*/ `
+    <form id="registerForm">
+			<div class="mb-3">
+				<label class="form-label" for="email">Email</label>
+				<input class="form-control" type="email" name="email" />
+			</div>
+
+			<div class="mb-3">
+				<label class="form-label" for="password">Password</label>
+				<input class="form-control" type="password" name="password" />
+			</div>
+
+			<div class="mb-3">
+				<button class="btn btn-primary w-100">Register</button>
+			</div>
+		</form>
 `;
 }
 
